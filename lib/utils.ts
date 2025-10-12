ede95290c64ab3ea97e79f9012f1d45fb6c7b058
gadm21 <g.gad@nu.edu.eg>
2025-10-12 15:07:43 -0400

    fix: update tailwind-merge import to use require

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge/dist/index";
- 
+const { twMerge } = require("tailwind-merge");
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
