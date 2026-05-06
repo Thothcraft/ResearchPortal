454579cc7f3dc644892984b6c9cf450e344ab6b5
gadm21 <g.gad@nu.edu.eg>
2026-05-06 01:22:46 -0400

    fix: add ThemeProvider to providers to fix settings page error

--- providers.tsx
+++ providers.tsx
@@ -2,15 +2,18 @@
 
 import { AuthProvider } from './contexts/AuthContext';
 import { ToastProvider } from './contexts/ToastContext';
+import { ThemeProvider } from './contexts/ThemeContext';
 import ChatBubble from './components/ChatBubble';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <AuthProvider>
-      <ToastProvider>
-        {children}
-        <ChatBubble />
-      </ToastProvider>
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <ToastProvider>
+          {children}
+          <ChatBubble />
+        </ToastProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
